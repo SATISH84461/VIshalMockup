9e07432e392762a711ee8c4cc8b41d3cb7657bb1
SATISH84461 <satish84461@gmail.com>
2025-07-27 20:16:06 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Home from './pages/Home.jsx';
 export default function App() {
 
   return (
-    <BrowserRouter basename="/VIshalMockup">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1 className="text-2xl font-bold text-center my-4">Vishal Mockup</h1>
+      <BrowserRouter basename="/VIshalMockup">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  basename: '/VIshalMockup',
+  base: '/VIshalMockup/',
   plugins: [react(), tailwindcss()],
 })
